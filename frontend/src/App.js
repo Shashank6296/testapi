f54fb58220957807902b85ae188e8327a0f31cc6
Shashank6296 <109429034+Shashank6296@users.noreply.github.com>
2025-01-17 00:00:06 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,11 +9,7 @@ const App = () => {
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-<<<<<<< HEAD
         const response = await axios.get('http://localhost:5000/api/events');
-=======
-        const response = await axios.get('https://dashboard.render.com/web/srv-cu4b89dds78s739qs2sg');
->>>>>>> 21d19b6cd2fb9e71eedecea651344c515aecfc6b
         setEvents(response.data);
       } catch (error) {
         console.error('Error fetching events:', error);
